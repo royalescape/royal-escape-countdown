04d97bf018f4cbf223aab64c1e597cb0595c09b0
rohan <rohan.doshi@bazaarvoice.com>
2026-01-06 18:41:51 +0530

    Fixing icon and countdown

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -10,7 +10,7 @@ const Countdown = () => {
   });
 
   useEffect(() => {
-    const targetDate = new Date("2026-01-01T00:00:00").getTime();
+    const targetDate = new Date("2026-02-02T00:00:00").getTime();
 
     const calculateTimeLeft = () => {
       const now = new Date().getTime();
