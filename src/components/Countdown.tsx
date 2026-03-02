e3e2ae39c195b47e0cee57f2ff0fd55e5f0058b7
rohan <rohan.doshi@bazaarvoice.com>
2026-03-02 15:56:17 +0530

    changing countdown to 15 March

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -10,7 +10,7 @@ const Countdown = () => {
   });
 
   useEffect(() => {
-    const targetDate = new Date("2026-02-26T00:00:00").getTime();
+    const targetDate = new Date("2026-03-15T00:00:00").getTime();
 
     const calculateTimeLeft = () => {
       const now = new Date().getTime();
